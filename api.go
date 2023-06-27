db09b167264cbddc46c4f75679a9a742d334af07
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2023-06-27 15:19:18 +0800

    feat: support most of the encoding

--- api.go
+++ api.go
@@ -1,12 +1,15 @@
 package twiscraper
 
 import (
+	"compress/flate"
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"time"
+
+	"github.com/andybalholm/brotli"
 )
 
 func (s *Scraper) requestAPI(req *http.Request, target interface{}) error {
@@ -31,6 +34,7 @@ func (s *Scraper) requestAPI(req *http.Request, target interface{}) error {
 	req.Header.Set("User-Agent", s.userAgent)
 	req.Header.Set("Authorization", "Bearer "+s.bearerToken)
 	req.Header.Set("X-Guest-Token", s.guestToken)
+	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
 
 	// use cookie
 	if s.IsLogined() {
@@ -44,8 +48,39 @@ func (s *Scraper) requestAPI(req *http.Request, target interface{}) error {
 	}
 	defer resp.Body.Close()
 
+	var content []byte
+	switch resp.Header.Get("Content-Encoding") {
+	case "gzip":
+		gr, err := gzip.NewReader(resp.Body)
+		if err != nil {
+			return err
+		}
+		defer gr.Close()
+		content, err = io.ReadAll(gr)
+		if err != nil {
+			return err
+		}
+	case "deflate":
+		fr := flate.NewReader(resp.Body)
+		defer fr.Close()
+		content, err = io.ReadAll(fr)
+		if err != nil {
+			return err
+		}
+	case "br":
+		br := brotli.NewReader(resp.Body)
+		content, err = io.ReadAll(br)
+		if err != nil {
+			return err
+		}
+	default:
+		content, err = io.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+	}
+
 	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusForbidden {
-		content, _ := ioutil.ReadAll(resp.Body)
 		return fmt.Errorf("request api failed %s: %s", resp.Status, content)
 	}
 
@@ -53,12 +88,8 @@ func (s *Scraper) requestAPI(req *http.Request, target interface{}) error {
 		s.guestToken = ""
 	}
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	s.sugar.Debugf("request api %s\n%s", req.URL, body)
-	return json.Unmarshal(body, target)
+	s.sugar.Debugf("request api %s\n%s", req.URL, content)
+	return json.Unmarshal(content, target)
 }
 
 func (s *Scraper) refreshGuestToken() error {
@@ -75,7 +106,7 @@ func (s *Scraper) refreshGuestToken() error {
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
--- go.mod
+++ go.mod
@@ -1,8 +1,11 @@
 module github.com/JasonKhew96/twiscraper
 
-go 1.19
+go 1.20
 
-require go.uber.org/zap v1.24.0
+require (
+	github.com/andybalholm/brotli v1.0.5
+	go.uber.org/zap v1.24.0
+)
 
 require (
 	go.uber.org/atomic v1.11.0 // indirect
